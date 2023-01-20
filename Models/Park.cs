903990cb9ac53dbf430b5f749558fab3c8b35cf5
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2023-01-20 11:14:57 -0800

    migrations not updating string length for type property. Throwing error, though validation has been updated and removed. Troubleshooting...

--- Models/Park.cs
+++ Models/Park.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ParksApi.Models
 {
   public class Park
   {
     public int ParkId { get; set; }
+    [Required]
+    [StringLength(60)]
     public string Name { get; set; }
+    [Required]
+    [StringLength(30)]
     public string Location { get; set; }
+    [Required]
+    [StringLength(15)]
     public string State { get; set; }
+    [Required]
+    [MaxLength(60)]
     public string Type { get; set; }
     public string Terrain { get; set; }
     public string Features { get; set; }
