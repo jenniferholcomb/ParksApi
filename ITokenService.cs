bacbf5592a31f14b78e9605fbe24e9f929196945
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2023-01-20 17:14:50 -0800

    successful in requiring authorization with api. Need to figure out how to log in. Thinking it would be on client side

--- ITokenService.cs
+++
@@ -1,9 +0,0 @@
-using ParksApi.Models;
-
-namespace ParksApi;
-
-public interface ITokenService
-{
-    string BuildToken(string key, string issuer, UserDTO user);
-    bool ValidateToken(string key, string issuer, string audience, string token);
-}
--- Program.cs
+++ Program.cs
@@ -5,122 +5,69 @@ using Microsoft.IdentityModel.Tokens;
 //using Microsoft.AspNetCore.Identity;
 using System.Text;
 
-namespace TravelApi
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ParksApiContext>(
+                dbContextOptions => dbContextOptions
+                    .UseMySql(
+                    builder.Configuration["ConnectionStrings:DefaultConnection"], 
+                    ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                    )
+                )
+                );
+
+//builder.Services.AddTransient<IParks, ParksRepository>();
+builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
-    class Program
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
     {
-        public IConfiguration Configuration { get; }
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 
-        public Program(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-        static void Main(string[] args)
-        {
+var app = builder.Build();
 
-            var builder = WebApplication.CreateBuilder(args);
+// Configure the HTTP request pipeline.
 
-            // Add services to the container.
-
-            builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.SaveToken = true;
-                options.RequireHttpsMetadata = false;
-                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidAudience = builder.Configuration["Jwt:Audience"], // can be set to anything
-                    ValidIssuer = builder.Configuration["Jwt:Issuer"], // can be set to anything
-                    ClockSkew = TimeSpan.Zero,// It forces tokens to expire exactly at token expiration time instead of 5 minutes later
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
-                    ValidateLifetime = false,
-                    ValidateIssuerSigningKey = true
-                };
-            });
-
-            //builder.Services.AddAuthorization();
-            //builder.Services.AddControllersWithViews();
-            builder.Services.AddControllers();
-            builder.Services.AddDbContext<ParksApiContext>(
-                            dbContextOptions => dbContextOptions
-                                .UseMySql(
-                                builder.Configuration["ConnectionStrings:DefaultConnection"], 
-                                ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
-                                )
-                            )
-                            );
-
-            builder.Services.AddTransient<IUserRepository, UserRepository>();
-            builder.Services.AddTransient<ITokenService, TokenService>();
-
-            builder.Services.AddEndpointsApiExplorer();
-            //builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseSession();
-            app.Use(async (context, next) =>    
-            {
-                var token = context.Session.GetString("Token"); 
-                if (!string.IsNullOrEmpty(token))  
-                {
-                    context.Request.Headers.Add("Authorization", "Bearer " + token);
-                }
-                await next();
-            });
-
-            //app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-            //app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-
-            app.Run();
-
-            // if (app.Environment.IsDevelopment())
-            // {
-            //     app.UseSwagger();
-            //     app.UseSwaggerUI();
-            // }
-            // else
-            // {
-            //     app.UseHttpsRedirection();
-            // }
-
-            // app.UseAuthentication();
-            // app.UseAuthorization();
-
-            // app.MapControllers();
-        }
-    }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+else
+{
+    app.UseHttpsRedirection();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.Run();
+
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+// else
+// {
+//     app.UseHttpsRedirection();
+// }
+
+// app.UseAuthentication();
+// app.UseAuthorization();
+
+// app.MapControllers();
 
